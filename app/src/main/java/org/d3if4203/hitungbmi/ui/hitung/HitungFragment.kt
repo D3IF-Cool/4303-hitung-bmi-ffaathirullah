537fc5f1bc2405c04db26820958d5ba8837050a1
Fachrul_Faathirullah <lenpuchongloi93@gmail.com>
2021-04-26 21:57:28 +0700

    Menyimpan data ke database

--- app/src/main/java/org/d3if4203/hitungbmi/ui/hitung/HitungFragment.kt
+++ app/src/main/java/org/d3if4203/hitungbmi/ui/hitung/HitungFragment.kt
@@ -3,21 +3,27 @@ package org.d3if4203.hitungbmi.ui.hitung
 import android.content.Intent
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
 import org.d3if4203.hitungbmi.R
 import org.d3if4203.hitungbmi.data.KategoriBmi
 import org.d3if4203.hitungbmi.databinding.FragmentHitungBinding
-
+import org.d3if4203.hitungbmi.db.BmiDb
 
 
 class HitungFragment : Fragment() {
     private lateinit var binding: FragmentHitungBinding
-    private val viewModel: HitungViewModel by viewModels()
+    private val viewModel: HitungViewModel by lazy {
+        val db = BmiDb.getInstance(requireContext())
+        val factory = HitungViewModelFactory(db.dao)
+        ViewModelProvider(this, factory).get(HitungViewModel::class.java)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -48,6 +54,12 @@ class HitungFragment : Fragment() {
                 getKategori(it.kategori))
             binding.buttonGroup.visibility = View.VISIBLE
         }
+
+        viewModel.data.observe(viewLifecycleOwner) {
+            if (it == null) return@observe
+            Log.d("HitungFragment", "Data tersimpan. ID = ${it.id}")
+        }
+
     }
 
     fun hitungBMI(){
--- app/src/main/java/org/d3if4203/hitungbmi/ui/hitung/HitungViewModel.kt
+++ app/src/main/java/org/d3if4203/hitungbmi/ui/hitung/HitungViewModel.kt
@@ -3,13 +3,20 @@ package org.d3if4203.hitungbmi.ui.hitung
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import org.d3if4203.hitungbmi.data.HasilBmi
 import org.d3if4203.hitungbmi.data.KategoriBmi
+import org.d3if4203.hitungbmi.db.BmiDao
+import org.d3if4203.hitungbmi.db.BmiEntity
 
-class HitungViewModel : ViewModel() {
+class HitungViewModel(private val db: BmiDao) : ViewModel() {
+
     private val hasilBmi = MutableLiveData<HasilBmi?>()
     private val navigasi = MutableLiveData<KategoriBmi?>()
-
+    val data = db.getLastBmi()
 
     fun hitungBmi(berat: String, tinggi: String, isMale: Boolean) {
         val tinggiCm = tinggi.toFloat() / 100
@@ -29,6 +36,18 @@ class HitungViewModel : ViewModel() {
             }
         }
         hasilBmi.value = HasilBmi(bmi, kategori)
+
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                val dataBmi = BmiEntity(
+                    berat = berat.toFloat(),
+                    tinggi = tinggi.toFloat(),
+                    isMale = isMale
+                )
+                db.insert(dataBmi)
+            }
+        }
+
     }
 
     fun mulaiNavigasi() {
